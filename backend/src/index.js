36537af7c89cfda7b943b2f9bfaa0e2f22b5b78a
blackcat-007 <shubhodeepmukherjee242gmail.com>
2025-02-01 09:31:56 +0530

    sidebar fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,13 +23,14 @@ app.use(cors({
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Production Setup
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
     });
 }
 
